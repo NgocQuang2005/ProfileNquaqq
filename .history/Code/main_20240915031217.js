647db1361e88f194578eec564ea5335d5d76d094
NgocQuang2005 <Quang111420@gmail.com>
2024-09-15 10:55:28 +0700

    profile cua nquangg

---
+++ .history/Code/main_20240915031217.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+  $(".project__link").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: `<button type='button' class='slick-prev pull-left'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
+    nextArrow: `<button type='button' class='slick-next pull-right'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
+    dots: true, //dấu chấm chuyển ảnh ở dưới
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 740,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+//scroll
+var animationElements = document.querySelectorAll(".show-on-scroll");
+function toggleAnimationElementInWindow(element) {
+  // getClientRects lấy ra tọa độ của diện tích thẻ đó , đây là domlist nên để là vị trí 0
+  var rect = element.getClientRects()[0];
+  // lấy chiều cao màn hình
+  var heightScreen = window.innerHeight;
+  // check xem element co ben trong man hinh hay khong
+  if (!(rect.bottom < 0 || rect.top > heightScreen)) {
+    // ben trong
+    element.classList.add("start");
+  } else {
+    //ben ngoai
+    element.classList.remove("start");
+  }
+}
+
+function checkAnimation() {
+  animationElements.forEach((el) => {
+    toggleAnimationElementInWindow(el);
+  });
+}
+window.onscroll = checkAnimation;
